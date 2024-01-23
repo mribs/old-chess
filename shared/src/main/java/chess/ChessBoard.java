759bf31eac9ffd415e29ef6f4f22b4909f45638f
mribs <maggieribble@gmail.com>
2024-01-22 23:03:55 -0700

    bishopBlocked test passes

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,8 +7,11 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    ChessPiece[][] board = new ChessPiece[8][8];
+
 
     public ChessBoard() {
+        //board constructor should set up the board
         
     }
 
@@ -19,8 +22,15 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
-    }
+        board[position.getRow() - 1][position.getColumn() - 1] = piece;
+////    System.out.println("added " + piece.getPieceType() + " at " + position.getRow() + ", " + position.getColumn());
+//        if (piece.getPieceType() == ChessPiece.PieceType.KING) {
+//            switch (piece.getTeamColor()) {
+//                case WHITE -> setWhiteKingPos(position);
+//                case BLACK -> setBlackKingPos(position);
+//            }
+////      System.out.println("king is at " + position.toString());
+        }
 
     /**
      * Gets a chess piece on the chessboard
@@ -30,7 +40,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow() - 1][position.getColumn() - 1];
     }
 
     /**
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Represents a single chess piece
@@ -9,8 +10,13 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    ChessGame.TeamColor color;
+    PieceType pieceType;
+
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.color = pieceColor;
+        this.pieceType = type;
     }
 
     /**
@@ -29,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return color;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return pieceType;
     }
 
     /**
@@ -47,6 +53,101 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> validMoves = new HashSet<>();
+        switch (pieceType) {
+            case BISHOP:
+                validMoves = bishopMoves(board, myPosition);
+                break;
+        }
+
+        return validMoves;
+    }
+
+    private Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> validMoves = new HashSet<>();
+        Collection<ChessPosition> validPositions = new HashSet<>();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        int tempColLeft = col;
+        int tempColRight = col;
+        ChessGame.TeamColor color = board.getPiece(myPosition).getTeamColor();
+
+        //White forward/ Black backwards
+        for (int r=row + 1; r <= 8; r++) {
+            //right
+            tempColRight=tempColRight + 1;
+            if (tempColRight <= 8 && tempColRight > 0) {
+                ChessPiece inFront=board.getPiece(new ChessPosition(r, tempColRight));
+                if (inFront == null) {
+                    validPositions.add(new ChessPosition(r, tempColRight));
+                } else if (inFront.getTeamColor() == color) {
+                    break;
+                } else {
+                    validPositions.add(new ChessPosition(r, tempColRight));
+                    break;
+                }
+            }
+        }
+        for (int r = row + 1; r <= 8; r++) {
+            //left
+            tempColLeft = tempColLeft - 1;
+            if (tempColLeft <= 8 && tempColLeft > 0) {
+                ChessPiece inFront=board.getPiece(new ChessPosition(r, tempColLeft));
+                if (inFront == null) {
+                    validPositions.add(new ChessPosition(r, tempColLeft));
+                }
+                else if (inFront.getTeamColor() == color) {
+                    break;
+                }
+                else {
+                    validPositions.add(new ChessPosition(r, tempColLeft));
+                    break;
+                }
+            }
+
+        }
+        //White Backward/ Black forwards
+        tempColRight = col;
+        tempColLeft = col;
+        for (int r=row - 1; r > 0; r--) {
+            //right
+            tempColRight=tempColRight + 1;
+            if (tempColRight <= 8 && tempColRight > 0) {
+                ChessPiece inFront=board.getPiece(new ChessPosition(r, tempColRight));
+                if (inFront == null) {
+                    validPositions.add(new ChessPosition(r, tempColRight));
+                } else if (inFront.getTeamColor() == color) {
+                    break;
+                } else {
+                    validPositions.add(new ChessPosition(r, tempColRight));
+                    break;
+                }
+            }
+        }
+        for (int r = row -1; r > 0; r--) {
+            //left
+            tempColLeft=tempColLeft - 1;
+            if (tempColLeft <= 8 && tempColLeft > 0) {
+                ChessPiece inFront = board.getPiece(new ChessPosition(r, tempColLeft));
+                if (inFront == null) {
+                    validPositions.add(new ChessPosition(r, tempColLeft));
+                } else if (inFront.getTeamColor() == color) {
+                    break;
+                } else {
+                    validPositions.add(new ChessPosition(r, tempColLeft));
+                    break;
+                }
+            }
+        }
+
+
+
+        if (validPositions.isEmpty()) return validMoves;
+        //make moves
+        for (ChessPosition position: validPositions) {
+            validMoves.add(new ChessMove(myPosition, position, null));
+        }
+
+        return validMoves;
     }
 }

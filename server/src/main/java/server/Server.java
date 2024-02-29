c2756d78dea82f6f9ec08f6333839bc2703d4571
mribs <maggieribble@gmail.com>
2024-02-28 23:12:18 -0700

    create user, join game

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -12,16 +12,16 @@ public class Server {
         // Register your endpoints and handle exceptions here.
 //        /create Routes
         Handler clearHandler = new ClearHandler();
-//        Handler createHandler = new CreateGameHandler();
-//        Handler joinHandler = new JoinGameHandler();
+        Handler createHandler = new CreateGameHandler();
+        Handler joinHandler = new JoinGameHandler();
 //        Handler listHandler = new ListGamesHandler();
 //        Handler loginHandler = new LoginHandler();
 //        Handler logoutHandler = new LogoutHandler();
 //        Handler registerHandler = new RegisterHandler();
 
         clearHandler.setupRoutes();
-//        createHandler.setupRoutes();
-//        joinHandler.setupRoutes();
+        createHandler.setupRoutes();
+        joinHandler.setupRoutes();
 //        listHandler.setupRoutes();
 //        loginHandler.setupRoutes();
 //        logoutHandler.setupRoutes();

6e32fbd75455ae60ee7e7d2083ddb8479c0f913c
mribs <maggieribble@gmail.com>
2024-02-28 23:11:52 -0700

    Added intended files, clearHandler

--- server/src/main/java/dataAccess/DAO/UserDAO.java
+++ server/src/main/java/dataAccess/DAO/UserDAO.java
@@ -1,4 +1,41 @@
 package dataAccess.DAO;
 
+import dataAccess.AlreadyTakenException;
+import dataAccess.BadRequestException;
+import dataAccess.DataAccessException;
+import dataAccess.models.User;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class UserDAO {
+  private static Map<String, User> userMap= new ConcurrentHashMap<>();
+  //creates new user
+  public void createUser(User u) throws DataAccessException, AlreadyTakenException, BadRequestException {
+    if (u == null || u.getUsername() == null) throw new BadRequestException();
+    if (userMap.containsKey(u.getUsername()) ) throw new AlreadyTakenException();
+
+    userMap.put(u.getUsername(), u);
+  }
+
+  //returns user information
+  public User readUser(String userName) throws DataAccessException {
+    if (userName == null) throw new DataAccessException("userName must not be null");
+
+    return userMap.get(userName);
+  }
+
+  //updates user information
+//  void updateUser(User u) throws DataAccessException{
+//  }
+
+  //deletes user
+  void deleteUser(User u) throws  DataAccessException {
+    userMap.remove(u);
+  }
+
+  //clear users
+  public void clearUsers() {
+    userMap.clear();
+  }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,31 @@
 package server;
 
+import dataAccess.handlers.*;
 import spark.*;
 
 public class Server {
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
-
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+//        /create Routes
+        Handler clearHandler = new ClearHandler();
+//        Handler createHandler = new CreateGameHandler();
+//        Handler joinHandler = new JoinGameHandler();
+//        Handler listHandler = new ListGamesHandler();
+//        Handler loginHandler = new LoginHandler();
+//        Handler logoutHandler = new LogoutHandler();
+//        Handler registerHandler = new RegisterHandler();
+
+        clearHandler.setupRoutes();
+//        createHandler.setupRoutes();
+//        joinHandler.setupRoutes();
+//        listHandler.setupRoutes();
+//        loginHandler.setupRoutes();
+//        logoutHandler.setupRoutes();
+//        registerHandler.setupRoutes();
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -19,4 +35,5 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
 }

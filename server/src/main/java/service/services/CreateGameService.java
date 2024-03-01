8c594ec57a7e07cefabab5754e8261c6cd17beda
mribs <maggieribble@gmail.com>
2024-03-01 12:54:07 -0700

    clear, createGame tests

--- server/src/main/java/service/services/CreateGameService.java
+++ server/src/main/java/service/services/CreateGameService.java
@@ -1,15 +1,21 @@
 package service.services;
 
+import dataAccess.Authorizer;
+import dataAccess.BadRequestException;
 import dataAccess.DAO.GameDAO;
 import dataAccess.DataAccessException;
+import dataAccess.UnauthorizedException;
 import dataAccess.models.Game;
 import service.requests.CreateGameRequest;
 import service.results.CreateGameResult;
 
 public class CreateGameService {
-  public CreateGameResult createGame(CreateGameRequest request) throws DataAccessException{
+  public CreateGameResult createGame(CreateGameRequest request) throws DataAccessException, UnauthorizedException, BadRequestException {
+   if (request.getGameName() == "") {
+     throw new BadRequestException();
+   }
     GameDAO gameDAO = new GameDAO();
-    //create game if authToken in header is valid
+
     Game game = gameDAO.insert(request.getGameName());
     return new CreateGameResult(game.getGameID());
   }
--- server/src/test/java/serviceTests/ServiceTest.java
+++ server/src/test/java/serviceTests/ServiceTest.java
@@ -1,10 +1,21 @@
 package serviceTests;
 
-import dataAccess.Database;
+import dataAccess.*;
+import dataAccess.models.AuthToken;
+import dataAccess.models.Game;
+import dataAccess.models.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import service.requests.CreateGameRequest;
+import service.requests.JoinGameRequest;
+import service.results.CreateGameResult;
+import service.results.JoinGameResult;
+import service.services.ClearService;
+import service.services.CreateGameService;
+import service.services.JoinGameService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,30 +23,52 @@ class ServiceTest {
 
   @BeforeEach
   void setUp() {
-    //set up database
-    Database database = new Database();
-    //add 1 user
 
+    //add 1 user
+    TempDatabase.userMap.put("testUser", new User("testUser", "testPass", "test"));
+    TempDatabase.authTokenMap.put("testAuth", new AuthToken("testUser", "testAuth"));
     //add 1 game
+    TempDatabase.gameMap.put(1, new Game("testGame"));
+  }
+
+  @AfterEach
+  void takeDown() {
+    TempDatabase.gameMap.clear();
+    TempDatabase.authTokenMap.clear();
+    TempDatabase.userMap.clear();
   }
 
   @Test
   void clear() {
+    new ClearService().clear();
     //check for empty database
+    assertTrue(TempDatabase.authTokenMap.isEmpty());
+    assertTrue(TempDatabase.userMap.isEmpty());
+    assertTrue(TempDatabase.gameMap.isEmpty());
   }
 
   @Test
-  void createGamePass() {
-    //success
+  void createGamePass() throws DataAccessException, UnauthorizedException, BadRequestException {
+    CreateGameResult result = new CreateGameService().createGame(new CreateGameRequest("new Game"));
+    System.out.println(result.toString());
+
+    assertTrue(TempDatabase.gameMap.containsKey(result.getGameID()));
   }
 
   @Test
   void createGameFail() {
+    assertThrows(BadRequestException.class, () -> {
+      //test no game name
+      CreateGameResult result = new CreateGameService().createGame(new CreateGameRequest(""));
+    });
 
   }
 
   @Test
-  void joinPass() {
+  void joinPass() throws BadRequestException, DataAccessException, AlreadyTakenException {
+    JoinGameResult result = new JoinGameService().join(new JoinGameRequest(1, "white"), "testUser");
+    Game game = TempDatabase.gameMap.get(1);
+    assertTrue(game.getWhiteUsername() == "testUser");
 
   }
   @Test

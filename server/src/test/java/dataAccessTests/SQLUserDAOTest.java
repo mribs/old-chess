4ce419dbb50bb34208283a02798e89e24023cc96
mribs <maggieribble@gmail.com>
2024-04-02 00:47:03 -0600

    userDAO tests

--- server/src/test/java/dataAccessTests/SQLUserDAOTest.java
+++ server/src/test/java/dataAccessTests/SQLUserDAOTest.java
@@ -2,8 +2,10 @@ package dataAccessTests;
 
 import dataAccess.AlreadyTakenException;
 import dataAccess.BadRequestException;
+import dataAccess.DAO.SQL.AuthDAO;
 import dataAccess.DAO.SQL.UserDAO;
 import dataAccess.DataAccessException;
+import dataAccess.models.AuthToken;
 import dataAccess.models.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,15 +13,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQLUserDAOTest {
+  UserDAO userDAO = new UserDAO();
   @BeforeEach
   void clearUserTable() {
-    UserDAO userDAO = new UserDAO();
     userDAO.clearUsers();
   }
 
   @Test
   void createUserTest() {
-        UserDAO userDAO = new UserDAO();
         try {
           userDAO.createUser(new User("testName", "testPass", "testEmail"));
         } catch (BadRequestException e) {
@@ -37,5 +38,30 @@ class SQLUserDAOTest {
           throw new RuntimeException(e);
         }
   }
+  @Test
+  void dontCreateUserTest() {
+    assertThrows(BadRequestException.class, () -> {
+      userDAO.createUser(null);
+    });
+  }
+
+  @Test
+  void readUserTest() throws BadRequestException, AlreadyTakenException, DataAccessException {
+    userDAO.createUser(new User("test", "pass", "email"));
+
+    User user = userDAO.readUser("test");
+    assertEquals("test", user.getUsername());
+  }
+  @Test
+  void readnullUserTest() throws DataAccessException {
+      assertNull(userDAO.readUser("i'm not real"));
+  }
+
+  @Test
+  void clearUserTest() {
+    assertDoesNotThrow(() -> userDAO.clearUsers());
+  }
+
+
 
 }
